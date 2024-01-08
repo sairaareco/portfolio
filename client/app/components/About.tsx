383ed636e2f818a381de06e65018b8bf3eaaec98
sairaareco <sairaareco@gmail.com>
2024-01-08 17:11:58 -0300

    creacion componentes

---
+++ client/app/components/About.tsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+
+    return(
+        <main>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam doloremque sapiente mollitia dolor doloribus officiis rerum sequi laboriosam repellendus ipsam optio minima accusamus labore, totam unde nam soluta nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nulla quia incidunt officiis? Magni voluptatem reprehenderit facilis dicta nam ipsa dolorem hic, unde beatae perferendis cupiditate assumenda, laborum, nostrum aliquid.</p>
+        </main>
+    )
+};
+
+export default About;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.css'
 import Navbar from './components/Navbar'
 import Init from './components/Init'
+import About from './components/About'
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <Navbar />
       <Init />
+      <About />
     </main>
   )
 }

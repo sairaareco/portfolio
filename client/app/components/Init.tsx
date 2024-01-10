8da0d55c307711e17aa318c0b8388c86b1aab789
sairaareco <sairaareco@gmail.com>
2024-01-10 13:13:05 -0300

    modificacion de componente init

--- client/app/components/Init.tsx
+++ client/app/components/Init.tsx
@@ -4,12 +4,20 @@ const Init = () => {
 
     return(
         <main className={styles.containerInit}>
-            <h3 className={styles.fullStack}> Full Stack </h3>
-            <h1 className={styles.developer}> Developer </h1>
+            <div className={styles.containerGrid}>
+                <div className={styles.containerBubble}>
+                    <h1 className={styles.name}> Saira </h1>
+                    <h1 className={styles.lastname}> Areco </h1>
+                </div>
 
-            <button className={styles.buttonDownloadCv}> Descarga mi CV </button>
+                <div>
+                    <h3 className={styles.fullStack}> Full Stack </h3>
+                    <h1 className={styles.developer}> Developer </h1>
+                    <button className={styles.buttonDownloadCv}> Descarga mi CV </button>
+                    <p className={styles.description}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iste officiis expedita nam id accusamus molestias eos ut iure rem corporis nulla, quasi, repudiandae error at consectetur eligendi! Eveniet, nesciunt.</p>
+                </div>
+            </div>
 
-            <p className={styles.description}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iste officiis expedita nam id accusamus molestias eos ut iure rem corporis nulla, quasi, repudiandae error at consectetur eligendi! Eveniet, nesciunt.</p>
 
             <div> </div>
             <div> </div>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,17 +2,13 @@ import styles from './page.module.css'
 import Navbar from './components/Navbar'
 import Init from './components/Init'
 import About from './components/About'
-import Model from './components/Model'
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-      <div className={styles.containerComponents}>
-        <Model />
-        <Init />
-      </div>
+      <Init />
       <About />
       
     </main>

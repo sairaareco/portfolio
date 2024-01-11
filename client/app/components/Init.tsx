d48655e30af595849a92c5de2e1d8ccd10576b9e
sairaareco <sairaareco@gmail.com>
2024-01-11 12:54:20 -0300

    continuacion de inicio

--- client/app/components/Init.tsx
+++ client/app/components/Init.tsx
@@ -5,11 +5,12 @@ const Init = () => {
     return(
         <main className={styles.containerInit}>
             <div className={styles.containerGrid}>
-                <div className={styles.containerBubble}>
+                <div className={styles.containerBubbleName}>
+                <div className={styles.bubble}>
                     <h1 className={styles.name}> Saira </h1>
                     <h1 className={styles.lastname}> Areco </h1>
                 </div>
-
+                </div>
                 <div>
                     <h3 className={styles.fullStack}> Full Stack </h3>
                     <h1 className={styles.developer}> Developer </h1>
@@ -17,12 +18,12 @@ const Init = () => {
                     <p className={styles.description}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iste officiis expedita nam id accusamus molestias eos ut iure rem corporis nulla, quasi, repudiandae error at consectetur eligendi! Eveniet, nesciunt.</p>
                 </div>
             </div>
-
-
-            <div> </div>
-            <div> </div>
-            <div> </div>
-            <div> </div>
+            <div className={styles.boxes}>
+                    <div className={styles.boxUno}> caja 1 </div>
+                    <div className={styles.boxDos}> caja 2 </div>
+                    <div className={styles.boxTres}> caja 3 </div>
+                    <div className={styles.boxCuatro}> caja 4 </div>
+                </div>
         </main>
     )
 };

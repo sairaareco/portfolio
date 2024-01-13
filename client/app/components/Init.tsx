1476cba8e409c3ac2eaf5b741e29b6df878b71a5
sairaareco <sairaareco@gmail.com>
2024-01-13 20:44:41 -0300

    cambios en portfolio

--- client/app/components/Init.tsx
+++ client/app/components/Init.tsx
@@ -1,11 +1,14 @@
 import styles from "../styles/Init.module.css";
 
+
 const Init = () => {
 
     return(
         <main className={styles.containerInit} id="init">
             <h2 className={styles.fullStack}> full stack </h2>
+            <div className={styles.containerDev}>
             <h1 className={styles.developer}>  developer  </h1>
+            </div>
         </main>
     )
 };

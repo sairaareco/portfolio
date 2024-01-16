146cce975eb2806679015be6dde50578755178bc
sairaareco <sairaareco@gmail.com>
2024-01-16 11:13:12 -0300

    continuacion del componente about e incorporacion de logos

--- client/app/components/About.tsx
+++ client/app/components/About.tsx
@@ -3,27 +3,35 @@ import styles from "../styles/About.module.css";
 import { useEffect, useState } from "react";
 
 const About = () => {
-// const [ showBorder, setShowBorder ] = useState(false);
-
-// useEffect(()=> {
-//   setTimeout(() => {
-//     setShowBorder(true);
-//   }, 2000);
-// }, []);
 
     return(
         <main className={styles.containerAbout} id="about">
-          <div className={styles.gradientAbout}>
-            <h2 className={styles.titleAbout}> SOBRE MÍ </h2>
-          </div>
-          {/* <h1 className={styles.onMoveDescription}> developer ... </h1> */}
-          <div className={styles.aboutP}>
-            <p className={styles.p}> ¡Hola! Mi nombre es Saira. Soy una desarrolladora Full Stack nacida en Argentina. </p>
-            <p className={styles.p}> Actualmente, me encuentro inmersa en la Tecnicatura en Análisis de Sistemas, donde estoy consolidando las bases de mis conocimientos teóricos y prácticos. </p>
-            <p className={styles.p}> Además, mi enfoque actual es fortalecer mis habilidades en el backend, centrándome en el lenguaje Java. </p>
-            <p className={styles.p}>Mi compromiso es continuar perfeccionando mi perfil tecnológico, explorando nuevas tecnologías y contribuyendo al desarrollo de soluciones eficientes e innovadoras. </p>
-          </div>
-        </main>
+          <section className={styles.grid}>
+            <div>
+              <div className={styles.gradientAbout}>
+                <h2 className={styles.titleAbout}> SOBRE MÍ </h2>
+              </div>
+              <div className={styles.aboutText}>
+                <p className={styles.p}> ¡Hola! Mi nombre es Saira. Soy una desarrolladora Full Stack nacida en Argentina. </p>
+                <p className={styles.p}> Actualmente, me encuentro inmersa en la Tecnicatura en Análisis de Sistemas, donde estoy consolidando las bases de mis conocimientos teóricos y prácticos. </p>
+                <p className={styles.p}> Además, mi enfoque actual es fortalecer mis habilidades en el backend, centrándome en el lenguaje Java. </p>
+                <p className={styles.p}>Mi compromiso es continuar perfeccionando mi perfil tecnológico, explorando nuevas tecnologías y contribuyendo al desarrollo de soluciones eficientes e innovadoras. </p>
+              </div>
+            </div>
+
+            <div className={styles.socialButtons}>
+              <button className={styles.buttonDownload}> Descarga mi CV </button>
+              <div className={styles.socialMedia}>
+                <a href="https://github.com/sairaareco" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/github.svg" alt="logo de github" width={25} className={styles.imageGithub}/>
+                </a>
+                <a href="https://www.linkedin.com/in/saira-areco/" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/linkedin.svg" alt="logo de linkedin" width={25} className={styles.imageLinkedin}/>
+                </a>       
+              </div>
+            </div>
+          </section>
+        </main>       
     )
 };
 
@@ -31,10 +39,3 @@ export default About;
 
 
 
-/*
-  <p className={styles.p}> ¡Hola! Mi nombre es Saira. Soy una desarrolladora Full Stack nacida en Argentina. </p>
-            <p className={styles.p}> Actualmente, me encuentro inmersa en la Tecnicatura en Análisis de Sistemas, donde estoy consolidando las bases de mis conocimientos teóricos y prácticos. </p>
-            <p className={styles.p}> Mi motivación para embarcarme en esta carrera radica en mi compromiso con el establecimiento de sólidas bases algorítmicas y en comprender a fondo los principios de la ciencia de la computación. </p>
-            <p className={styles.p}> Además, mi enfoque actual es fortalecer mis habilidades en el backend, centrándome en el lenguaje Java. Esta experiencia no solo está ampliando mi conocimiento, sino que también me permite enfrentar desafíos del mundo real. </p>
-            <p className={styles.p}>Mi compromiso es continuar perfeccionando mi perfil tecnológico, explorando nuevas tecnologías y contribuyendo al desarrollo de soluciones. </p>
-*/

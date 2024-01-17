f96c4f77b3ade9f69d44b79d8edd16ee6f8cdff1
sairaareco <sairaareco@gmail.com>
2024-01-17 12:42:46 -0300

    creacion del form en componente contacto

--- client/app/components/Contact.tsx
+++ client/app/components/Contact.tsx
@@ -6,7 +6,26 @@ const Contact = () => {
         <main id="contact" className={styles.containerContact}>
             <h2 className={styles.titleContact}> CONTACTO </h2>
 
-            <form action=""></form>
+            <form action="" className={styles.form}>
+                <div className={styles.personalInfoContainer}>          
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="name" className={styles.label}> NOMBRE </label>
+                        <input type="text" id="name"/>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="email" className={styles.label}> EMAIL </label>
+                        <input type="text" id="email"/>
+                        </div>
+                </div>
+
+                <div className={styles.messageContainer}>
+                    <label htmlFor="subject" className={styles.label}> ASUNTO </label>
+                    <input type="text" id="subject"/>
+
+                    <label htmlFor="message" className={styles.label}> MENSAJE </label>
+                    <input type="text" id="message"/>
+                </div>
+            </form>
         </main>
     )
 };

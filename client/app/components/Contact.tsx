1d3f06608323bd3df5fdc4ef4bb8314fd5e90339
sairaareco <sairaareco@gmail.com>
2024-01-17 21:50:57 -0300

    estilos en form

--- client/app/components/Contact.tsx
+++ client/app/components/Contact.tsx
@@ -5,34 +5,34 @@ const Contact = () => {
     return(
         <main id="contact" className={styles.containerContact}>
             <h2 className={styles.titleContact}> CONTACTO </h2>
-
             <form action="" className={styles.form}>
                 <section className={styles.formContent}>
                     <div className={styles.personalInfoContainer}>          
                         <div className={styles.inputGroup}>
-                            <label htmlFor="name" className={styles.label}> NOMBRE </label>
+                            <label htmlFor="name" className={styles.labelName}> NOMBRE </label>
                             <input type="text" 
                             id="name" 
                             className={styles.inputData}/>
                         </div>
                         <div className={styles.inputGroup}>
-                            <label htmlFor="email" className={styles.label}> EMAIL </label>
+                            <label htmlFor="email" className={styles.labelEmail}> EMAIL </label>
                             <input type="text" 
                             id="email" 
                             className={styles.inputData}/>
                         </div>
                     </div>
 
-                    <div className={styles.messageContainer}>
-                        <label htmlFor="subject" className={styles.label}> ASUNTO </label>
+                    <div className={styles.messageContainer}>          
+                        <label htmlFor="subject" className={styles.labelSubject}> ASUNTO </label>
                         <input type="text" 
                         id="subject" 
                         className={styles.input}/>
-
-                        <label htmlFor="message" className={styles.label}> MENSAJE </label>
+                    
+                        <label htmlFor="message" className={styles.labelMessage}> MENSAJE </label>
                         <input type="text" 
                         id="message" 
                         className={styles.inputMessage}/>
+                        
                     </div>          
                 </section>
                 <button className={styles.buttonSubmit}> ENVIAR </button>

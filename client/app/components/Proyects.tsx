3e42f2c11e744bfb8486469371478658905d3eb5
sairaareco <sairaareco@gmail.com>
2024-01-17 23:01:54 -0300

    cambios en contactps

--- client/app/components/Proyects.tsx
+++ client/app/components/Proyects.tsx
@@ -2,10 +2,12 @@ import styles from "../styles/Proyects.module.css";
 
 const Proyects = () => {
 
-
     return (
         <main className={styles.containerProyect} id="proyects">
             <h2 className={styles.titleProyect}> PROYECTOS </h2>
+            <section>
+            <p></p>
+            </section>
         </main>
     )
 };
